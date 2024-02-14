dd1f78fa2d1792d05ce4af4299886c2df83f89ae
AnaCP7 <anacapper24@hgmail.com>
2024-02-14 19:31:57 +0100

    unido login con back y registro

--- DietAI-frontend/src/app/components/form-login/form-login.component.ts
+++ DietAI-frontend/src/app/components/form-login/form-login.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent {
+  username: string='';
+  password: string='';
+
+  hacerLogin() {
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.username,
+        password: this.password
+      })
+    })
+  }
 
 }
