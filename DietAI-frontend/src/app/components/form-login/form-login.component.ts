5e34cd7e738e9d0a68bbc367639241ff6f03bb2c
AnaCP7 <anacapper24@hgmail.com>
2024-02-15 13:20:13 +0100

    ahora login devuelve objeto en vez de json

--- DietAI-frontend/src/app/components/form-login/form-login.component.ts
+++ DietAI-frontend/src/app/components/form-login/form-login.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-form-login',
-  templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.css']
+    selector: 'app-form-login',
+    templateUrl: './form-login.component.html',
+    styleUrls: ['./form-login.component.css'],
 })
 export class FormLoginComponent {
-  username: string='';
-  password: string='';
+    username: string = '';
+    password: string = '';
 
-  hacerLogin() {
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: this.username,
-        password: this.password
-      })
-    })
-  }
+    hacerLogin() {
+        const formData = new FormData();
+        formData.append('username', this.username);
+        formData.append('password', this.password);
 
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            body: formData,
+        })
+            .then((response) => {
+                // Handle response
+            })
+            .catch((error) => {
+                // Handle error
+            });
+    }
 }
