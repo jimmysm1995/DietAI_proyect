a20df6fba8dc5e7248bd92880b27eccdb4c9982d
josesm <jose.s.m.1995@gmail.com>
2024-02-16 12:25:21 +0100

    commit cambio loggin

--- DietAI-frontend/src/app/components/form-login/form-login.component.ts
+++ DietAI-frontend/src/app/components/form-login/form-login.component.ts
@@ -1,28 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-    selector: 'app-form-login',
-    templateUrl: './form-login.component.html',
-    styleUrls: ['./form-login.component.css'],
+  selector: 'app-form-login',
+  templateUrl: './form-login.component.html',
+  styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent {
-    username: string = '';
-    password: string = '';
 
-    hacerLogin() {
-        const formData = new FormData();
-        formData.append('username', this.username);
-        formData.append('password', this.password);
+  errorMessage: string = '';
+  @ViewChild('userForm') userForm!: NgForm;
+  constructor(private userService: UserService, private router: Router) {}
 
-        fetch('http://localhost:3000/login', {
-            method: 'POST',
-            body: formData,
-        })
-            .then((response) => {
-                // Handle response
-            })
-            .catch((error) => {
-                // Handle error
-            });
-    }
+  logearUsuario(userData: User): void {
+    this.userService.loginUser(userData).subscribe(
+      (response: any) => {
+        console.log('User logged in successfully');
+        // Redirigir al usuario a la página de inicio
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.error('Error logging in user:', error);
+        // Mostrar mensaje de error en el clienteSS
+        console.log(error);
+        this.errorMessage = error; // error.error contiene el mensaje de error del backend
+        this.userForm.reset();
+      }
+    );
+  }
 }
