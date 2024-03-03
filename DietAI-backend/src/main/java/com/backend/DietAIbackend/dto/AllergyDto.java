2d47f44a5a39a802b51422a492863cab51b18708
josesm <jose.s.m.1995@gmail.com>
2024-03-03 11:45:23 +0100

    "Mejora del servicio de token y de la entidad client"

---
+++ DietAI-backend/src/main/java/com/backend/DietAIbackend/dto/AllergyDto.java
@@ -0,0 +1,11 @@
+package com.backend.DietAIbackend.dto;
+
+import com.backend.DietAIbackend.model.AllergyType;
+import com.backend.DietAIbackend.model.Client;
+
+public class AllergyDto {
+
+    private AllergyType type;
+
+    private ClientDto clientDto;
+}
