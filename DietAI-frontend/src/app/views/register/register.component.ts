8546e4770ef2e5a96b67d463c800ed6470dea04c
AnaCP7 <anacapper24@gmail.com>
2024-05-20 22:02:54 +0200

    Made login and register pages responsive for phone tablet and big screens

--- DietAI-frontend/src/app/views/register/register.component.ts
+++ DietAI-frontend/src/app/views/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  ngOnInit() {
+    this.muestra();
+  }
+  muestra(){
+    console.log("tamaño ancho: " + window.innerWidth);
+    console.log("tamaño altura: " + window.innerHeight);
+  }
 
 }
