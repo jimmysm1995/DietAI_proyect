2ae15b277991a49ad920f3da0d94c6683d07224f
josesm <jose.s.m.1995@gmail.com>
2024-06-14 17:22:42 +0200

    Mejora css resposible

--- DietAI-frontend/src/app/components/entrenameinto/entrenameinto.component.ts
+++ DietAI-frontend/src/app/components/entrenameinto/entrenameinto.component.ts
@@ -5,59 +5,57 @@ import { ClientService } from 'src/app/services/client.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
-  selector: 'app-entrenameinto',
-  templateUrl: './entrenameinto.component.html',
-  styleUrls: ['./entrenameinto.component.css']
+    selector: 'app-entrenameinto',
+    templateUrl: './entrenameinto.component.html',
+    styleUrls: ['./entrenameinto.component.css'],
 })
 export class EntrenameintoComponent implements AfterViewInit {
-  public training: TrainingResponse = new TrainingResponse();
-  public trainings: TrainingResponse[] = [];
-  public dias: number[] = [];
-  public errorMessage = '';
+    public training: TrainingResponse = new TrainingResponse();
+    public trainings: TrainingResponse[] = [];
+    public dias: number[] = [];
+    public errorMessage = '';
 
-  constructor(
-    private trainingService: TrainingService,
-    private userService: UserService,
-    private clientService: ClientService
-  ) {
-  }
+    constructor(
+        private trainingService: TrainingService,
+        private userService: UserService,
+        private clientService: ClientService
+    ) {}
 
-  ngAfterViewInit(): void {
-    this.errorMessage = '';
+    ngAfterViewInit(): void {
+        this.errorMessage = '';
 
-    this.clientService.getCurrentClient().then((user: any) => {
+        this.clientService.getCurrentClient().then((user: any) => {
+            this.userService.getClient(user.idUser).then((client) => {
+                this.clientService
+                    .getTrainingByClient(client.idClient || 0)
+                    .then((training) => {
+                        if (!training) {
+                            this.errorMessage =
+                                'No se han encontrado entrenamientos.';
+                            return;
+                        }
 
-      this.userService.getClient(user.idUser).then((client) => {
+                        this.trainingService
+                            .getById(training.idTraining || 0)
+                            .then((trainings) => {
+                                this.dias = trainings
+                                    .map((training) => training.orderWeek)
+                                    .filter((valor, indice, array) => {
+                                        return array.indexOf(valor) === indice;
+                                    })
+                                    .sort();
+                                this.trainings = trainings;
+                            });
+                    });
+            });
+        });
+    }
 
-        this.clientService.getTrainingByClient(client.idClient || 0).then((training) => {
+    mostrarEntrenamiento(training: TrainingResponse) {
+        this.training = training;
+    }
 
-          if (!training) {
-            this.errorMessage = 'No se han encontrado entrenamientos.';
-            return;
-          }
-
-
-          this.trainingService.getById(training.idTraining || 0).then((trainings) => {
-            this.dias = trainings.map((training) => training.orderWeek).filter((valor, indice, array) => {
-              return array.indexOf(valor) === indice;
-            }).sort();
-            this.trainings = trainings
-          })
-
-        })
-
-      })
-      
-    })
-
-  }
-
-  mostrarEntrenamiento(training: TrainingResponse) {
-    this.training = training
-  }
-
-  clearErrorMessage() {
-    this.errorMessage = '';
-  }
-
+    clearErrorMessage() {
+        this.errorMessage = '';
+    }
 }
